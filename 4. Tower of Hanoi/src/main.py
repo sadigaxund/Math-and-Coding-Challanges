b397f24baba22d7085f36e1872608ff9c1ec00ab
Sadig <sadigaxund@gmail.com>
2021-09-19 00:53:36 +0400

    Project Done

--- 4. Tower of Hanoi/src/main.py
+++ 4. Tower of Hanoi/src/main.py
@@ -36,14 +36,46 @@ settings = config['COMPONENTS']
 colors = config['COLORS']
 fetchInt = lambda key : int(settings[key])
 
-############################ <------- START GUI -------> ############################
 G2D = Renderer((fetchInt('WINDOW_WIDTH'), fetchInt('WINDOW_HEIGHT')), 'Tower of Hanoi') 
-manager = pygame_gui.UIManager(G2D.WINDOW_SIZE)
+MANAGER = pygame_gui.UIManager(G2D.WINDOW_SIZE)
+
+############################ <------- START GUI -------> ############################
 MARGIN = fetchInt('MARGIN')
+MENU_HEIGHT = 40
+SLIDER_WIDTH = 200
+LABEL_WIDTH = 30
+LABEL_HEIGHT = MENU_HEIGHT - MARGIN * 2 
+LABEL_Y = (MENU_HEIGHT - LABEL_HEIGHT) / 2 - 3
+SLIDER_Y = LABEL_Y - 1
+BUTTON_SIZE = MENU_HEIGHT * 0.65
+
+
+MENU_BORDERS = G2D.Rect(0, 0, G2D.WINDOW_SIZE[0], MENU_HEIGHT)
+MENU = pygame_gui.elements.UIPanel(MENU_BORDERS, 4, MANAGER)
 
 
 
+LABEL_BORDER =  G2D.Rect(MARGIN * 2, LABEL_Y, LABEL_WIDTH * 3, LABEL_HEIGHT)
+diskLabel = pygame_gui.elements.UILabel(LABEL_BORDER, "Disks: 3", MANAGER, MENU)
 
+BUTTON_BORDER = G2D.Rect(LABEL_BORDER.right + MARGIN * 2, (MENU_HEIGHT - BUTTON_SIZE)/2 - 3, BUTTON_SIZE, BUTTON_SIZE)
+upBtn = pygame_gui.elements.UIButton(BUTTON_BORDER, '↑', MANAGER, MENU,'#up')
+
+BUTTON_BORDER = G2D.Rect(BUTTON_BORDER.right + MARGIN, (MENU_HEIGHT - BUTTON_SIZE)/2 - 3, BUTTON_SIZE, BUTTON_SIZE)
+downBtn = pygame_gui.elements.UIButton(BUTTON_BORDER, '↓', MANAGER, MENU,'#down')
+
+LABEL_BORDER =  G2D.Rect(MENU_BORDERS.right - MARGIN * 6 - LABEL_BORDER.width, LABEL_Y, LABEL_WIDTH * 3, LABEL_HEIGHT)
+minimumLabel = pygame_gui.elements.UILabel(LABEL_BORDER, "Minimum: 7", MANAGER, MENU)
+
+LABEL_BORDER =  G2D.Rect(LABEL_BORDER.left - MARGIN * 3 - LABEL_BORDER.width, LABEL_Y, LABEL_WIDTH * 3, LABEL_HEIGHT)
+movesLabel = pygame_gui.elements.UILabel(LABEL_BORDER, "Moves: 0", MANAGER, MENU)
+BUTTON_SIZE *= 1.1
+BUTTON_BORDER = G2D.Rect(G2D.WINDOW_SIZE[0]/2 - BUTTON_SIZE * 2, (MENU_HEIGHT - BUTTON_SIZE)/2 - 2, BUTTON_SIZE * 4, BUTTON_SIZE)
+restartBtn = pygame_gui.elements.UIButton(BUTTON_BORDER, 'Restart', MANAGER, MENU,'#restart')
+
+LABEL_BORDER =  G2D.Rect(G2D.WINDOW_SIZE[0]/2 - 100, MENU_BORDERS.height+20, 200, 100)
+winLbl = pygame_gui.elements.UILabel(LABEL_BORDER, "Solved!", MANAGER)
+winLbl.hide()
 ############################## <------- END GUI -------> ############################
 
 
@@ -51,11 +83,13 @@ MARGIN = fetchInt('MARGIN')
 ############################## <------- START GAME -------> #########################
 
 towerSize = (200, 200)
-y = G2D.WINDOW_SIZE[1] / 2 - towerSize[1] / 2
+y = G2D.WINDOW_SIZE[1] / 2 - towerSize[1] / 2 + MENU_BORDERS.height
 xMid = G2D.WINDOW_SIZE[0] / 2 - towerSize[0] / 2
 someMargin = 100
 # Varibale that holds the instance of Disk, when any was popped
 DISK_ON_HAND = None
+DISKS_AMOUNT = 3
+MOVES = 0
 # List of Towers
 towers =    [  
                 Tower(G2D, G2D.Rect(xMid - towerSize[0] - someMargin, y, towerSize[0], towerSize[1])), 
@@ -71,11 +105,9 @@ def initTowers(startAmount):
             tower.pop()
 
     for i in range(0, startAmount):
-        towers[1].addDisk(8 - (startAmount - i - 1))
+        towers[0].addDisk(8 - (startAmount - i - 1))
 
-initTowers(8)
-
-
+initTowers(DISKS_AMOUNT)
 
 def draw(this):
     # Fill Background with the given color in config.ini file
@@ -88,9 +120,12 @@ def draw(this):
         DISK_ON_HAND.draw()
 
 
+GAMEOVER = False
 
 def clickTower(this, event):
-    global DISK_ON_HAND
+    global DISK_ON_HAND, MOVES, GAMEOVER
+    if GAMEOVER:
+        return
     if event.type == G2D.PYGAME_INSTANCE_.MOUSEBUTTONDOWN and event.button == 1:
         l = len(towers) # You could just put 3 here, it is need for future if the number of towers can change
         for i in range(0, l):
@@ -112,6 +147,7 @@ def clickTower(this, event):
                     if abs(diff) < towers[i].hitbox.width:
                         # negative means tower has bigger disk
                         if size_diff < 0:
+                            MOVES += 1
                             towers[i].push(DISK_ON_HAND) # add current disk to the tower
                             DISK_ON_HAND = None # no more current disk
                     else: # if the different tower was clicked
@@ -119,18 +155,41 @@ def clickTower(this, event):
                 break
 
 def handleEvent(this, event):
-    manager.process_events(event)
+    global DISKS_AMOUNT, MOVES, GAMEOVER
+    MANAGER.process_events(event)
+    if event.type == G2D.PYGAME_INSTANCE_.USEREVENT:
+        if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == upBtn:
+                if DISKS_AMOUNT < 8:
+                    DISKS_AMOUNT += 1
+            if event.ui_element == downBtn:
+                if DISKS_AMOUNT > 0:
+                    DISKS_AMOUNT -= 1
+            if event.ui_element in {restartBtn, upBtn, downBtn}:
+                MOVES = 0
+                initTowers(DISKS_AMOUNT)
+                GAMEOVER = False
+                winLbl.hide()
+
+    
     # For future, you can add dragging instead of clicking
     clickTower(this, event)
+    if towers[len(towers) - 1].size() == DISKS_AMOUNT:
+        winLbl.show()
+        GAMEOVER = True
     
+
     
 
 
 
 def update(this):
-    manager.update(G2D.clock.get_time()/1000.0)
+    MANAGER.update(G2D.clock.get_time()/1000.0)
     G2D.WINDOW.blit(G2D.WINDOW, (0, 0))
-    manager.draw_ui(G2D.WINDOW)
+    MANAGER.draw_ui(G2D.WINDOW)
+    minimumLabel.set_text("Minimum: " + str(pow(2, DISKS_AMOUNT) - 1))
+    diskLabel.set_text("Disks: " + str(DISKS_AMOUNT))
+    movesLabel.set_text("Moves: " + str(MOVES))
     pass
 
 G2D.setEventLogic(handleEvent)
