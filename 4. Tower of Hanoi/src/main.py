be2ea5400bf67d096ef01ad21818435659841d3f
Sadig <sadigaxund@gmail.com>
2021-09-18 14:40:15 +0400

    Pop & Push Mechanism implemented

--- 4. Tower of Hanoi/src/main.py
+++ 4. Tower of Hanoi/src/main.py
@@ -41,22 +41,21 @@ MARGIN = fetchInt('MARGIN')
 
 class Disk:
     def __init__(self, G2D, bounds, index, color = None) -> None:
-        self.bounds = bounds
-        self.pos = (bounds.top, bounds.left)
-        self.size = (bounds.width, bounds.height)
+        self.pos = [bounds.left, bounds.top]
+        self.size = [bounds.width, bounds.height]
         self.G2D = G2D
-        self.index = index
+        self.index = index # index defines the color and the width of the Disk
         self.color = G2D.PEN.GREEN if color == None else color
         pass
     def draw(self):
-        self.G2D.PEN.drawRoundRect(G2D.Rect(self.bounds.left, self.bounds.top,
-                                     self.bounds.width, self.bounds.height), 5, self.color, stroke = 0)
+        self.G2D.PEN.drawRoundRect(G2D.Rect(self.pos[0], self.pos[1],
+                                     self.size[0], self.size[1]), 5, self.color, stroke = 0)
         pass
+    def __str__(self) -> str:
+        return "Disk: " + "{Index:" + str(self.index) + ", Color:" + str(self.color) + ", Position:" + str(self.pos) + ", Size:" + str(self.size) + "}"
 
 class Tower:
-    DISK_AMOUNT = 0
-    DISKS:Disk = []
-
+ 
     def __init__(self, G2D, bounds) -> None:
         self.rod_width = int(bounds.width * 0.06)
         self.rod_height = int(bounds.height*1.35)
@@ -65,36 +64,41 @@ class Tower:
         self.disk_height = int(bounds.height * 0.15)
         self.bounds = bounds
         self.G2D = G2D
+        # self.DISK_AMOUNT = 0
+        self.DISKS = []
 
     def addDisk(self, index = None, size = None, color = None):
-        self.DISK_AMOUNT = self.DISK_AMOUNT + 1
-        index = self.DISK_AMOUNT if index == None else index + 1
-        t =  (10 - index + 1) / 15
+        disks_amount = len(self.DISKS) + 1 # the number of disks after adding the disk, needed for calculation
+        index = disks_amount + 1 if index == None else index + 1 
+        t =  (10 - index + 1) / 15 # coefficient that defines the width and the color of the disk
 
         w = self.base_width * t
         h = self.disk_height
         (w, h) = (w, h) if size == None else size
 
         x = self.bounds.left + (self.base_width - w) / 2
-        y = self.bounds.bottom - self.base_height - self.disk_height * self.DISK_AMOUNT 
+        y = self.bounds.bottom - self.base_height - self.disk_height * disks_amount
         
         color = self.G2D.PEN.colorOnRainbow(t * 3 / 2 + 0.1) if color == None else color
         self.DISKS.append(Disk(self.G2D, self.G2D.Rect(x, y, w, h), index, color))
         
         
     def pop(self):
-        self.DISK_AMOUNT = self.DISK_AMOUNT - 1
+        if(not bool(self.DISKS)):
+            return None
         return self.DISKS.pop()
 
     def push(self, disk):
+        print(disk)
         self.addDisk(size = disk.size, color = disk.color)
-        
-        
+    
+    
     
     def draw(self):
         # Draw Rod
-        self.G2D.PEN.drawRoundRect(G2D.Rect(self.bounds.left + (self.bounds.width - self.rod_width)/2 - 1, self.bounds.bottom - self.rod_height - self.base_height + 5,
-                                    self.rod_width, self.rod_height), 2, "#FFFFFF", 0)
+        self.ROD = G2D.Rect(self.bounds.left + (self.bounds.width - self.rod_width)/2 - 1, self.bounds.bottom - self.rod_height - self.base_height + 5,
+                                    self.rod_width, self.rod_height)
+        self.G2D.PEN.drawRoundRect(self.ROD, 2, "#FFFFFF", 0)
         # Draw Disks
         for disk in self.DISKS:
             disk.draw()
@@ -102,6 +106,17 @@ class Tower:
         self.G2D.PEN.drawRoundRect(G2D.Rect(self.bounds.left, self.bounds.bottom - self.base_height, self.bounds.width, self.base_height), 2, "#964B00", 0)
 
         pass
+    def disksToString(self):
+        retval = "[ ----- " 
+        for disk in self.DISKS:
+            retval += str(disk) + ", "
+
+        return retval[:-2] + " ----- ]" 
+
+    def __str__(self) -> str:
+        return "Tower: {Size: "+ str(len(self.DISKS)) + ", Disks:" + self.disksToString() + "}"
+    
+    
 
 tower_size = (200, 100)
 
@@ -119,61 +134,70 @@ towers =    [
 
 def initTowers(startAmount):
     for tower in towers:
-        while tower.DISK_AMOUNT > 0:
+        while bool(tower.DISKS):
             tower.pop()
 
     for i in range(0, startAmount):
-        towers[0].addDisk(8 - (startAmount - i - 1))
+        towers[1].addDisk(8 - (startAmount - i - 1))
 
 initTowers(3)
 
-# rod1 = Rod(G2D, (100, 100), ROD_LENGTH)
+DISK_ON_HAND = None
+
 def draw(this):
     G2D.PEN.fillBackground(colors['BACKGROUND'])
     for tower in towers:
         tower.draw()
+    
+    if DISK_ON_HAND != None:
+        DISK_ON_HAND.draw()
+        pass
+    # x = self.bounds.left + (self.base_width - w) / 2
 
-DRAGING = False
 
-def handleTowerDragging(tower:Tower, pos):
-    global DRAGING, offset_x, offset_y, mouse_x, mouse_y, diskToDrag
-    for disk in tower.DISKS:
-        if disk.bounds.collidepoint(pos):
-            print(disk)
-            DRAGING = True
-            mouse_x, mouse_y = pos
-            offset_x = disk.bounds.x - mouse_x
-            offset_y = disk.bounds.y - mouse_y
-            diskToDrag = disk
-            break
-def handleDiskDragging(pos):
-    global DRAGING, offset_x, offset_y, mouse_x, mouse_y, diskToDrag
-    if DRAGING:
-        mouse_x, mouse_y = pos
-        diskToDrag.x = mouse_x + offset_x
-        diskToDrag.y = mouse_y + offset_y
 
+def clickTower(this, event):
+    global DISK_ON_HAND
+    if event.type == G2D.PYGAME_INSTANCE_.MOUSEBUTTONDOWN and event.button == 1:
+        l = len(towers)
+        for i in range(0, l):
+            # if player clicks on one of tower
+            if towers[i].bounds.collidepoint(event.pos):
+                if DISK_ON_HAND == None:
+                    DISK_ON_HAND = towers[i].pop()
+                    # If player tries to pop from the empty Tower
+                    if DISK_ON_HAND != None:
+                        DISK_ON_HAND.pos[1] = towers[i].ROD.top - DISK_ON_HAND.size[1] * 2
+                    break
+                else:
+                    # if the same tower was clicked
+                    diff = towers[i].bounds.x - DISK_ON_HAND.pos[0]
+                    if abs(diff) < towers[i].bounds.width:
+                        towers[i].push(DISK_ON_HAND)
+                        DISK_ON_HAND = None
+                    # if the different tower was clicked
+                    else:
+                        DISK_ON_HAND.pos[0] = towers[i].bounds.left + (towers[i].base_width - DISK_ON_HAND.size[0]) / 2
+                break
+
+                   
+                    
 
 def handleEvent(this, event):
     manager.process_events(event)
-    if event.type == G2D.PYGAME_INSTANCE_.MOUSEBUTTONDOWN and event.button == 1:
-        for tower in towers:
-            if tower.bounds.collidepoint(event.pos):
-                print(tower)
-                handleTowerDragging(tower, event.pos)
-                break
+    clickTower(this, event)
+    
     if event.type == G2D.PYGAME_INSTANCE_.MOUSEBUTTONUP:
             global DRAGING
             if event.button == 1:            
                 DRAGING = False
     if event.type == G2D.PYGAME_INSTANCE_.MOUSEMOTION:
-        handleDiskDragging(event.pos)
+        
         pass
 
 
 
 def update(this):
-    global CANVAS_BORDER, BOTTOM_MENU_BORDERS, bottom_panel
     manager.update(G2D.clock.get_time()/1000.0)
     G2D.WINDOW.blit(G2D.WINDOW, (0, 0))
     manager.draw_ui(G2D.WINDOW)
---
+++ 4. Tower of Hanoi/src/test.py
@@ -0,0 +1,7 @@
+hey = [1, 2, 3, 4, 5, 6]
+print(hey.pop())
+print(hey.pop())
+print(hey.pop())
+print(hey.pop())
+print(hey.pop())
+print(hey.pop())
